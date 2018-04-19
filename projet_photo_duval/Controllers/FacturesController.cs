1147d6bc0004545d80339565e7b9e538bbac070c
1567473 <1567473@LLBD0603INFO18>
2018-04-19 09:41:03 -0400

    MVC pour Facture

---
+++ projet_photo_duval/Controllers/FacturesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using projet_photo_duval.Models;
+
+namespace projet_photo_duval.Controllers
+{
+    public class FacturesController : Controller
+    {
+        private H18_Proj_Eq07Entities1 db = new H18_Proj_Eq07Entities1();
+
+        // GET: Factures
+        public ActionResult Index()
+        {
+            var facture = db.Facture.Include(f => f.Seance);
+            return View(facture.ToList());
+        }
+
+        // GET: Factures/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Facture facture = db.Facture.Find(id);
+            if (facture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(facture);
+        }
+
+        // GET: Factures/Create
+        public ActionResult Create()
+        {
+            ViewBag.Seance_ID = new SelectList(db.Seance, "Seance_ID", "DateSeance");
+            return View();
+        }
+
+        // POST: Factures/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Facture_ID,Seance_ID,Prix,EstPayee")] Facture facture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Facture.Add(facture);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Seance_ID = new SelectList(db.Seance, "Seance_ID", "Adresse", facture.Seance_ID);
+            return View(facture);
+        }
+
+        // GET: Factures/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Facture facture = db.Facture.Find(id);
+            if (facture == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Seance_ID = new SelectList(db.Seance, "Seance_ID", "Adresse", facture.Seance_ID);
+            return View(facture);
+        }
+
+        // POST: Factures/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Facture_ID,Seance_ID,Prix,EstPayee")] Facture facture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(facture).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Seance_ID = new SelectList(db.Seance, "Seance_ID", "Adresse", facture.Seance_ID);
+            return View(facture);
+        }
+
+        // GET: Factures/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Facture facture = db.Facture.Find(id);
+            if (facture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(facture);
+        }
+
+        // POST: Factures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Facture facture = db.Facture.Find(id);
+            db.Facture.Remove(facture);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
